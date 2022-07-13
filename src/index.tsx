608946c28b0ee710816d3f73d518f6a3bb2f11c8
paulvalenzuela <paul.valenzuela@bcstechnology.com.au>
2022-07-13 23:18:47 +0800

    [UPDATE] - route for App

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import App from 'pages/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
